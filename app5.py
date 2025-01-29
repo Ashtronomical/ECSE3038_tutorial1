44384021dc5b66f1f9064b90d66bae2fd9c0d557
Ashleigh Alexander <ashleighalexander07@gmail.com>
2025-01-28 19:26:45 -0500

    first commit

---
+++ app5.py
@@ -0,0 +1,7 @@
+squares = [] 
+even_numbers = []
+upper_case_names = []
+
+# a. Starting with an empty list, append the squares for the numbers from 1 to 5 using a for loop.
+# b. Starting with an empty list, append the even numbers from 2 to 10 (inclusive) using a for loop.
+# c. Using a for loop, create a list of names in uppercase from the list of people in Exercise 3.
